eaddfcbf1259296aff5fba4716738bdb62bf85e0
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-09-05 23:39:38 -0600

    desplegar pagina http desde express

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
+
 const cors = require('cors');
+require('dotenv').config();
 
 //Crear servidor / aplicacion de express
 const app = express();
 
-//CORS
+//  CORS
 app.use( cors() );
-
+//  Directorio publico
+app.use( express.static('public') );
 //  Lectura y parseo del body
 app.use( express.json() );
 
 //  Routes - middleware
 app.use('/api/auth', require('./routes/auth'));
 
-app.listen(4000, () =>{
-    console.log(`Servidor corriendo en puerto ${4000}...`)
+app.listen( process.env.PORT , () =>{
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}...`)
 });
